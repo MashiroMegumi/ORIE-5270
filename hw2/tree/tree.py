e9f37b63541a2aca3d896a1426bc77c57961699f
MashiroMegumi <41775720+MashiroMegumi@users.noreply.github.com>
2018-08-23 11:14:42 -0400

    Update tree.py

--- hw2/tree/tree.py
+++ hw2/tree/tree.py
@@ -1,23 +1,24 @@
-# the node class for tree construction     
+# the node class for tree construction
 class Node:
     def __init__(self, n):
         """
         param n: the value of the node
         """
-        self.value = n # set the node value, without specific left child and right child
+        self.value = n  # set the node value, without specific left child and right child
         self.l = None
         self.r = None
-    def addlc(self, l): # the method to add a left child
+        
+    def addlc(self, l):  # the method to add a left child
         self.l = l
-    def addrc(self, r): # the method to add a right child
+        
+    def addrc(self, r):  # the method to add a right child
         self.r = r
-    def getheight(node): # the static method to find the "height" of the tree consisting of nodes.
+        
+    def getheight(node):  # the static method to find the "height" of the tree consisting of nodes.
         if node is None:
             return 0
         else:
-            return 1+max(Node.getheight(node.l),Node.getheight(node.r))
-
-
+            return 1+max(Node.getheight(node.l), Node.getheight(node.r))
 
 # the binary tree class
 class Btree:
@@ -26,7 +27,7 @@ class Btree:
         root is a node with reference to its children
         """
         self.root = root
-        self.height = Node.getheight(root) #get the tree height
+        self.height = Node.getheight(root)  #get the tree height
         
         # initialize the matrix holding the results
         self.matrix = [["|" for i in range(2**self.height-1)] for j in range(self.height)] 
@@ -50,7 +51,7 @@ class Btree:
                 self.ptree(node.r, index, r, h+1, matrix)
       
     def pring(self):
-        self.ptree(self.root, 0, 2**self.height-1, 0, self.matrix) # the default settings
+        self.ptree(self.root, 0, 2**self.height-1, 0, self.matrix)  # the default settings
         for i in self.matrix:
             print(i)
         return (self.matrix)
